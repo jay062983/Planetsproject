163b05d0a1971293a235025ad985c85c40895897
jay062983 <jaycs0617@gmail.com>
2023-03-28 20:43:46 -0400

    Planets adding

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        Planets planets=new Planets();
+        planets.planetOne="1";
+        planets.planetTwo="2";
+        planets.planetThree="3";
+
+    }
+
+}
---
+++ src/Planets.java
@@ -0,0 +1,6 @@
+public class Planets {
+    String planetOne;
+    String planetTwo;
+    String planetThree;
+
+}
